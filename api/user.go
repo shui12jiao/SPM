c1f250694d2081b740a936698cfeac3dc89ad6eb
shui12jiao <mengsiming77@gmail.com>
2025-04-03 15:50:29 +0800

    实现user api

--- api/user.go
+++ api/user.go
@@ -150,3 +150,140 @@ func (server *Server) updateMe(ctx *gin.Context) {
 
 	ctx.JSON(http.StatusOK, newUserResponseFromUser(user))
 }
+
+// listUser列出用户
+
+type listUserRequest struct {
+	Pagination
+	// 可选参数
+	Role       *string `form:"role" binding:"omitempty"`
+	Department *string `form:"department" binding:"omitempty"`
+}
+
+func (server *Server) listUser(ctx *gin.Context) {
+	var req listUserRequest
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.ListUserParams{
+		Limit:      req.PageSize,
+		Offset:     (req.Page - 1) * req.PageSize,
+		Role:       db.ToNull[sql.NullString](req.Role),
+		Department: db.ToNull[sql.NullString](req.Department),
+	}
+
+	users, err := server.store.ListUser(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	var userList []userResponse
+	for _, user := range users {
+		userList = append(userList, newUserResponseFromUser(user))
+	}
+
+	// 返回用户列表
+	ctx.JSON(http.StatusOK, userList)
+}
+
+// getUser获取用户信息
+// GET /admin/user/:id
+
+type getUserRequest struct {
+	ID int32 `uri:"id" binding:"required,min=1"`
+}
+
+func (server *Server) getUser(ctx *gin.Context) {
+	var req getUserRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	user, err := server.store.GetUser(ctx, req.ID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, newUserResponseFromUser(user))
+}
+
+// updateUser更新用户信息
+// PATCH /admin/user/:id
+
+type updateUserRequest struct {
+	ID         int32   `uri:"id" binding:"required,min=1"`
+	Username   *string `json:"username" binding:"omitempty"`
+	Password   *string `json:"password" binding:"omitempty,password"`
+	Role       *string `json:"role" binding:"omitempty"`
+	Department *string `json:"department" binding:"omitempty"`
+	Email      *string `json:"email" binding:"omitempty,email"`
+}
+
+func (server *Server) updateUser(ctx *gin.Context) {
+	var req updateUserRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.UpdateUserParams{
+		ID:         req.ID,
+		Username:   db.ToNull[sql.NullString](req.Username),
+		Password:   db.ToNull[sql.NullString](req.Password),
+		Role:       db.ToNull[sql.NullString](req.Role),
+		Department: db.ToNull[sql.NullString](req.Department),
+		Email:      db.ToNull[sql.NullString](req.Email),
+	}
+
+	user, err := server.store.UpdateUser(ctx, arg)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, newUserResponseFromUser(user))
+}
+
+// deleteUser删除用户信息
+// DELETE /admin/user/:id
+
+type deleteUserRequest struct {
+	ID int32 `uri:"id" binding:"required,min=1"`
+}
+
+func (server *Server) deleteUser(ctx *gin.Context) {
+	var req deleteUserRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	err := server.store.DeleteUser(ctx, req.ID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, nil)
+}
